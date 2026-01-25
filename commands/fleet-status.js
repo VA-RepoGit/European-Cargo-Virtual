bc31832891e863f8abdb3079495448cc756dc0d1
VA-RepoGit <europeancargovirtual@protonmail.com>
2026-01-25 20:50:05 +0100

    Create fleet-status.js

---
+++ commands/fleet-status.js
@@ -0,0 +1,5 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('fleet_status')
+  .setDescription('Affiche l\'état de maintenance de toute la flotte');
