1aee4b8b06b1907af7c46e3e61d0a00a3f53d4cb
VA-RepoGit <europeancargovirtual@protonmail.com>
2026-02-20 22:52:05 +0100

    Update fleet-status.js

--- commands/fleet-status.js
+++ commands/fleet-status.js
@@ -2,5 +2,5 @@ import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
   .setName('fleet_status')
-  .setDescription('Display detailed maintenance status (A, B, C) for the entire fleet')
+  .setDescription('Display detailed maintenance status (A, B, C, D) for the entire fleet')
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
